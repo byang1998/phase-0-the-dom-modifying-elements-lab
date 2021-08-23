adc70542750ee1d88436c5b602d669f615d43b87
Ben Yang <benyang2020@gmail.com>
2021-08-23 04:57:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+document.querySelector("h1#victory");
+newHeader.innerHTML ="<h1>Ben is the champion</h1>"
+
+mainid = document.querySelector("#main").remove();
